7b7a0be44184fc18b739de283bea95b5f5d1009b
Seren9 <shashkov_serega@mail.ru>
2023-12-06 13:21:19 +0300

    Solution 1

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
 public class Egg6 {
+	
+	private Needle7 needle7;
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle7.toString();
     }
+
+	public Egg6(Needle7 needle7) {
+		super();
+		this.needle7 = needle7;
+	}
+    
+    
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,13 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {	
 
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
     }
+    
 }
